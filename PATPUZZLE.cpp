7e20c03f66a4b4bd96abb260a7e4690b779ce1dd
cara-r-craw <88162925+cara-r-craw@users.noreply.github.com>
2021-07-29 15:31:26 -0400

    Add files via upload

---
+++ PATPUZZLE.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double code1;
+	cout << "Welcome to this PAT PUZZLE. " << endl;
+	cout << "Here is your first puzzle: https://drive.google.com/file/d/15i8sl6MiyKdOehE_WZwdHzAEzQuBQzJp/view?usp=sharing " << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 16356)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number two: https://drive.google.com/file/d/1bg1a7Efo8PR0_sABOx0xVJcsnNXXoqlj/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 98869)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number three: https://drive.google.com/file/d/1LwFX4WL0ml0Kno7Wi2Rd_6Xt0YjLuCpF/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 13798)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number four: https://drive.google.com/file/d/1lIr0i0y2MArqcnucWubN4KIb0z3jV-01/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 78129)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number five: https://drive.google.com/file/d/1hmeEVgv5iFvSQ7NePkZ0-De_ZorxE2ji/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 37914)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number six: https://drive.google.com/file/d/1vayn-9eqO-qYj0V6JwaW_QNZkmgYVjsD/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 68132)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number 7: https://drive.google.com/file/d/1eHFoqwbMIV5eM4mSp-9f8vXW-BFNuqUz/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 73218)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is puzzle number 8: https://drive.google.com/file/d/1DbGixOkq_nTCx4bKMN1cL0ZUAI-yV5zR/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1; 
+	cout << endl;
+	
+	while (code1 != 65864)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "You are correct! " << endl << endl;
+	cout << "Here is the final puzzle! https://drive.google.com/file/d/1Qgea7dFqPXGwnwCbdFi_aSIniCQmiIWe/view?usp=sharing" << endl;
+
+	cout << "What is the code? " << endl;
+	cout << "Enter the code: ";
+	cin >> code1;
+	cout << endl;
+
+	while (code1 != 82457)
+	{
+		cout << "You are incorrect! Try again. " << endl;
+		cout << "Enter the code: ";
+		cin >> code1;
+		cout << endl;
+	}
+
+	cout << "CONGRATULATIONS! You did it! :) " << endl;
+	cout << "Dad and I (and many analyticals) love sudoku puzzles.... " << endl;
+	cout << "I know sudoku puzzles aren't exactly your favorite, so I am proud of you! " << endl << endl;
+
+	cout << "BONUS QUESTION: What was John's favorite number? " << endl;
+	do
+	{
+		cout << "Enter the number: ";
+		cin >> code1;
+	} while (code1 != 7);
+
+	cout << "Correct! " << endl;
+
+	return 0;
+}
